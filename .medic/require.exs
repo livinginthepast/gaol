2e9527e7b1f6515d8398a71b4452800de43726b5
Eric Saxby <sax@livinginthepast.org>
2023-01-03 19:40:21 +0000

    Add medic

---
+++ .medic/require.exs
@@ -0,0 +1,3 @@
+Mix.install([
+  {:medic, "~> 1.8.4", force: true}
+])
--- mix.exs
+++ mix.exs
@@ -17,6 +17,10 @@ defmodule Gaol.MixProject do
 
   defp deps,
     do: [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.29", only: [:dev], runtime: false},
+      {:mix_audit, "~> 2.1", only: :dev, runtime: false},
       {:rustler, "~> 0.26.0"}
     ]
 end
