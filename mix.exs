9d87de9fb49ae9af586a0268b7cfb01438838f76
Eric Saxby <sax@livinginthepast.org>
2023-01-08 05:31:33 +0000

    Add package configuration

--- mix.exs
+++ mix.exs
@@ -1,6 +1,10 @@
 defmodule Gaol.MixProject do
   use Mix.Project
 
+  @description "FreeBSD jails"
+  @scm_url "https://github.com/livinginthepast/gaol"
+  @version "0.1.0"
+
   def application,
     do: [
       extra_applications: [:logger]
@@ -10,9 +14,15 @@ defmodule Gaol.MixProject do
     do: [
       app: :gaol,
       deps: deps(),
+      description: @description,
+      docs: docs(),
       elixir: "~> 1.14",
+      homepage_url: @scm_url,
+      package: package(),
+      preferred_cli_env: [credo: :test, dialyzer: :test, docs: :dev],
+      source_url: @scm_url,
       start_permanent: Mix.env() == :prod,
-      version: "0.1.0"
+      version: @version
     ]
 
   defp deps,
@@ -22,5 +32,27 @@ defmodule Gaol.MixProject do
       {:ex_doc, "~> 0.29", only: [:dev], runtime: false},
       {:mix_audit, "~> 2.1", only: :dev, runtime: false},
       {:rustler, "~> 0.26.0"}
+    ]
+
+  defp docs,
+    do: [
+      main: "readme",
+      source_ref: "v#{@version}"
+    ]
+
+  defp package,
+    do: [
+      licenses: ["MIT"],
+      maintainers: ["Eric Saxby"],
+      links: %{"GitHub" => @scm_url},
+      files: ~w(
+        LICENSE.md
+        README.md
+        lib
+        mix.exs
+        native/gaol_nif/src
+        native/gaol_nif/Cargo.toml
+        native/gaol_nif/Cargo.lock
+      )
     ]
 end
