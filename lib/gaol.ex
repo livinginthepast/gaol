c94612d364100092dc3c5853194d6c128f7c0912
Eric Saxby <sax@livinginthepast.org>
2023-01-08 23:48:19 +0000

    Add top-level functions in Gaol module

--- lib/gaol.ex
+++ lib/gaol.ex
@@ -2,4 +2,21 @@ defmodule Gaol do
   @moduledoc """
   Documentation for `Gaol`.
   """
+  alias Gaol.Native
+
+  @doc "List all running jails"
+  @spec all() :: [Gaol.Jail.t()]
+  def all, do: Native.all()
+
+  @doc "Start a new jail with the given path and name"
+  @spec create(Path.t(), binary()) :: {:ok, Gaol.Jail.t()} | {:error, atom()}
+  def create(path, name), do: Native.create(path, name)
+
+  @doc "Finds a jail by jid, or returns {:error, :not_found}"
+  @spec get(Gaol.Jail.jid()) :: {:ok, Gaol.Jail.t()} | {:error, :not_found}
+  def get(jid), do: Native.find_jail(jid)
+
+  @doc "Stops a jail by jid, or returns {:error, :not_found}"
+  @spec kill(Gaol.Jail.jid()) :: :ok | {:error, :not_found}
+  def kill(jid), do: Native.kill(jid)
 end
